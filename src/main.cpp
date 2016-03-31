c9617c776bc0df08561e2069043332a9a54acba1
xentec <andrej.utz@st.oth-regensburg.de>
2016-03-31 14:32:04 +0200

    Move specialized functions into util.h

--- src/main.cpp
+++ src/main.cpp
@@ -57,42 +57,6 @@ void Win32_Break()
 #endif
 }
 
-std::vector<string> splitString(string str, char at)
-{
-	std::vector<u32> positions;
-	std::vector<string> retMe;
-	int count = 0;
-
-	if (str.empty())
-	{
-		PrintError("Split string failed during empty input !", "splitString => if (str.empty()) ..");
-		return retMe;
-	}
-
-	positions.push_back(0);
-	for (char& c : str)
-	{
-		if (c == at)
-		{
-			positions.push_back(count);
-		}
-
-		count++;
-	}
-
-	for (int i = 0; i < positions.size() - 1; i++)
-	{
-		if (positions.at(i) == 0)
-			retMe.push_back(str.substr(positions.at(i), positions.at(i + 1)));
-		else
-			retMe.push_back(str.substr(positions.at(i) + 1, positions.at(i + 1) - positions.at(i) - 1));
-	}
-
-	retMe.push_back(str.substr(positions.at(positions.size() - 1) + 1, str.length() - positions.size() - 1));
-
-	return retMe;
-}
-
 bool parseStartupArguments(int argc, char* argv[], std::string& map_name)
 {
 	if (argc < 2)
---
+++ src/util.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "global.h"
+
+#include <vector>
+#include <istream>
+
+std::vector<string> splitString(string str, char at);
