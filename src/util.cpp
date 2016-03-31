d8dd8ba6528b56e8d9c18cd65029e62c2e0e74c8
xentec <andrej.utz@st.oth-regensburg.de>
2016-03-31 16:41:42 +0200

    Simplified splitString

--- src/util.cpp
+++ src/util.cpp
@@ -1,39 +1,18 @@
 #include "util.h"
 
+#include <stdexcept>
+#include <sstream>
+
 std::vector<string> splitString(string str, char at)
 {
-	std::vector<u32> positions;
-	std::vector<string> retMe;
-	int count = 0;
+	std::stringstream stream(str);
+	std::string item;
+	std::vector<std::string> elems;
 
-	if (str.empty())
-	{
-		PrintError("Split string failed during empty input !", "splitString => if (str.empty()) ..");
-		return retMe;
+	while (std::getline(stream, item, at)) {
+		elems.push_back(item);
 	}
-
-	positions.push_back(0);
-	for (char& c : str)
-	{
-		if (c == at)
-		{
-			positions.push_back(count);
-		}
-
-		count++;
-	}
-
-	for (int i = 0; i < positions.size() - 1; i++)
-	{
-		if (positions.at(i) == 0)
-			retMe.push_back(str.substr(positions.at(i), positions.at(i + 1)));
-		else
-			retMe.push_back(str.substr(positions.at(i) + 1, positions.at(i + 1) - positions.at(i) - 1));
-	}
-
-	retMe.push_back(str.substr(positions.at(positions.size() - 1) + 1, str.length() - positions.size() - 1));
-
-	return retMe;
+	return elems;
 }
 
 std::basic_istream<char> & readline(std::basic_istream<char> &stream, string &str)
