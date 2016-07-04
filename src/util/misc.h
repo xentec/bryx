c5bf3913eeba428ec913ef8c51a6da0937807301
xentec <andrej.utz@st.oth-regensburg.de>
2016-07-04 08:17:00 +0200

    util: added LowPass and RunningAverage counters

--- src/util/misc.h
+++ src/util/misc.h
@@ -11,3 +11,42 @@ std::basic_istream<char>& readline(std::basic_istream<char>& stream, string& str
 string readline(std::basic_istream<char>& stream);
 
 string toLower(string str);
+
+template<class T, usz SIZE = 5>
+struct LowPass
+{
+	usz i = 0;
+	std::array<T, SIZE> data;
+
+	void push(T&& val)
+	{
+		data[i] = val;
+		i = (i+1) % data.size();
+	}
+
+	T average() const
+	{
+		T sum{};
+
+		for(const T& v: data)
+			sum += v;
+
+		return sum / data.size();
+	}
+};
+
+template<class T>
+struct RunningAverage
+{
+	std::deque<T> data;
+
+	T average() const
+	{
+		T sum{};
+
+		for(const T& v: data)
+			sum += v;
+
+		return sum / data.size();
+	}
+};
